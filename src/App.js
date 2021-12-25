ac2f3246b7b6a3a6070b8319707e3b7b6f0fd359
Dylan Black <dylan.jacob.black@gmail.com>
2021-12-25 16:00:56 +0800

    add basic routing and individual lesson page

--- src/App.js
+++ src/App.js
@@ -1,14 +1,21 @@
 import { Fragment } from 'react';
+import { Route } from 'react-router-dom';
 import './App.module.css'
 
-import NavBar from "./components/NavBar/NavBar.js";
-import Lessons from "./components/Lessons/Lessons.js";
+import NavBar from "./components/NavBar/NavBar";
+import Lessons from "./components/Lessons/Lessons";
+import Lesson from './components/Lesson/Lesson';
 
 function App() {
   return (
     <Fragment>
       <NavBar />
-      <Lessons />
+      <Route path="/lessons">
+        <Lessons />
+      </Route>
+      <Route path="/lesson">
+        <Lesson />
+      </Route>
     </Fragment>
   );
 }
