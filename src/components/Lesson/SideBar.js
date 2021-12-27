85c79e263231a24f075d4b512ce565da9ebfbd1d
Dylan Black <dylan.jacob.black@gmail.com>
2021-12-27 15:09:44 +0800

    add dynamic routing and forward lessonid to SideBar thorugh params

--- src/components/Lesson/SideBar.js
+++ src/components/Lesson/SideBar.js
@@ -1,11 +1,13 @@
 import classes from "./SideBar.module.css";
 import dummyPic from "../Lessons/card-left.jpg";
 
-const SideBar = () => {
+const SideBar = (params) => {
   return (
     <div className={classes.sidebar}>
-      <div className={classes["lesson-pic"]}><img src={dummyPic} alt="dummy-pic" /></div>
-      <h3>Title Goes Here</h3>
+      <div className={classes["lesson-pic"]}>
+        <img src={dummyPic} alt="dummy-pic" />
+      </div>
+      <h3>{params.lessonInfo}</h3>
     </div>
   );
 };
--- src/pages/Lesson.js
+++ src/pages/Lesson.js
@@ -1,12 +1,15 @@
-import dummyPic from "../components/Lessons/card-left.jpg";
+import { useParams } from 'react-router-dom'
+
 import classes from "./Lesson.module.css";
 import SideBar from "../components/Lesson/SideBar"
 import LessonDisplay from "../components/Lesson/LessonDisplay"
 
 const Lesson = () => {
+  const params = useParams();
+
   return (
     <div className={classes.lesson}>
-      <SideBar />
+      <SideBar lessonInfo={params.lessonId}/>
       <LessonDisplay />
     </div>
   );
