d8629bac22b3fa46a98c0f68a782ae3ef81df10a
Dylan Black <dylan.jacob.black@gmail.com>
2021-12-28 07:48:42 +0800

    Generate lesson cards from database using  Rails API

--- src/pages/Lessons.js
+++ src/pages/Lessons.js
@@ -1,15 +1,24 @@
+import { useState, useEffect } from "react";
+
 import classes from "./Lessons.module.css";
 import Lesson from "../components/Lessons/Lesson";
 
 const Lessons = () => {
+  const [lessons, setLessons] = useState([]);
+
+  useEffect(() => {
+    fetch("http://localhost:3000/api/v1/lessons")
+      .then((response) => {
+        return response.json();
+      })
+      .then((data) => {
+        setLessons(data);
+      });
+  }, []);
+
   return (
     <div className={classes.lessons}>
-      <Lesson />
-      <Lesson />
-      <Lesson />
-      <Lesson />
-      <Lesson />
-      <Lesson />
+      {lessons.map(lesson => <Lesson title={lesson.title}/>)}
     </div>
   );
 };
