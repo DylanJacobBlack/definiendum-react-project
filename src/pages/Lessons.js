be121e888a753c35c20658427603d5a2d352537b
Dylan Black <dylan.jacob.black@gmail.com>
2021-12-29 08:42:18 +0800

    added error state to lessons page

--- src/pages/Lessons.js
+++ src/pages/Lessons.js
@@ -6,24 +6,49 @@ import LessonCard from "../components/Lessons/LessonCard";
 const Lessons = () => {
   const [lessons, setLessons] = useState([]);
   const [isLoading, setIsLoading] = useState(false);
+  const [error, setError] = useState(null);
 
   useEffect(() => {
     async function fetchLessonData() {
-      setIsLoading(true);
-      console.log('Loading!')
-      const response = await fetch("http://localhost:3000/api/v1/lessons");
-      const data = await response.json();
-      setLessons(data);
+      try {
+        setIsLoading(true);
+        setError(null);
+        console.log("Loading!");
+        const response = await fetch("http://localhost:3000/api/v1/lessons");
+
+        if (!response.ok) {
+          throw new Error("Something went wrong.");
+        }
+        const data = await response.json();
+        setLessons(data);
+        console.log("Done loading!");
+      } catch (error) {
+        setError(error.message);
+      }
       setIsLoading(false);
-      console.log('Done loading!')
     }
     fetchLessonData();
   }, []);
 
+  let status = "No lesson found.";
+
+  if (lessons.length > 0) {
+    status = "";
+  }
+
+  if (error) {
+    status = error;
+  }
+
+  if (isLoading) {
+    status = "Lessons loading...";
+  }
+
   return (
     <div className={classes.lessons}>
-      {isLoading && <h1>Lessons loading!!!</h1>}
+      {status !== "" && <h1>{status}</h1>}
       {!isLoading &&
+        lessons.length > 0 &&
         lessons.map((lesson) => <LessonCard title={lesson.title} />)}
     </div>
   );
