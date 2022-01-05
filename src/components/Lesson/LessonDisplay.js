5746f7236ffd946573779df9e5df64fb02678af0
Dylan Black <dylan.jacob.black@gmail.com>
2022-01-05 19:00:47 +0800

    Incorporate new code into real lessons page and use text fetched from the databaes

--- src/components/Lesson/LessonDisplay.js
+++ src/components/Lesson/LessonDisplay.js
@@ -1,4 +1,4 @@
-import { useState, useEffect } from "react";
+import React, { useRef, useEffect, useState } from "react";
 
 import classes from "./LessonDisplay.module.css";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
@@ -10,10 +10,112 @@ import {
 const LessonDisplay = (props) => {
   const [lessonPages, setLessonPages] = useState(null);
   const [currentPage, setCurrentPage] = useState(0);
+  const canvasRef = useRef();
+
+  const columnWidth = 400;
+  const columnHeight = 150;
+  const pagePaddingLeft = 10;
+  const pagePaddingRight = 10;
+  const approxWordsPerPage = 500;
+  const lineHeight = 18;
 
   useEffect(() => {
     if (props.isLoading === false && props.status === "") {
-      setLessonPages(props.text.split("."));
+    const pages = [];
+    const maxLinesPerPage = parseInt(columnHeight / lineHeight) - 1;
+    const x = pagePaddingLeft;
+    const y = lineHeight;
+    const maxWidth = columnWidth - pagePaddingLeft - pagePaddingRight;
+
+    // # words that have been displayed
+    //(used when ordering a new page of words)
+    let wordCount = 0;
+
+    const canvas = canvasRef.current;
+    const context = canvas.getContext("2d");
+    context.font = "14px verdana";
+
+    const getNextWords = (nextWordIndex) => {
+      const textWords = props.text.split(" ");
+      const words = textWords.splice(nextWordIndex, approxWordsPerPage);
+
+      return words;
+    };
+
+    const textToLines = (words, maxWidth, maxLines, x, y) => {
+      const lines = [];
+
+      while (words.length > 0 && lines.length <= maxLines) {
+        let line = getLineOfText(words, maxWidth);
+        words = words.splice(line.index + 1);
+        lines.push(line);
+        wordCount += line.index + 1;
+      }
+
+      return lines;
+    };
+
+    const getLineOfText = (words, maxWidth) => {
+      let line = "";
+      let space = "";
+      for (let i = 0; i < words.length; i++) {
+        let testWidth = context.measureText(line + " " + words[i]).width;
+        // When tested width is greater than the maxwidth, return an index of one less
+        if (testWidth > maxWidth) {
+          return { index: i - 1, text: line };
+        }
+        line += space + words[i];
+        space = " ";
+      }
+      return { index: words.length - 1, text: line };
+    };
+
+    const drawSvg = (lines, x, i) => {
+      const tspans = [];
+      for (let l = 0; l < lines.length; l++) {
+        const tspan = (
+          <tspan key={`page${i}-line${l}`} x={x} dy={`${lineHeight}px`}>
+            {lines[l].text}
+          </tspan>
+        );
+        tspans.push(tspan);
+      }
+      const sText = (
+        <text fontFamily="verdana" fontSize="14px" fill="#000000">
+          {tspans}
+        </text>
+      );
+
+      return (
+        <div
+          className={classes.column}
+          style={{ height: columnHeight, width: columnWidth }}
+          key={`page${i}`}
+        >
+          <svg
+            xmlns="http://www.w3.org/2000/svg"
+            height={columnHeight}
+            width={columnWidth}
+          >
+            {sText}
+          </svg>
+        </div>
+      );
+    };
+
+    for (let i = 0; i < 5; i++) {
+      console.log(`round ${i}`);
+      let lines = textToLines(
+        getNextWords(wordCount),
+        maxWidth,
+        maxLinesPerPage,
+        x,
+        y
+      );
+      pages.push(drawSvg(lines, x, i));
+    }
+
+    setLessonPages(pages);
     }
   }, [props.text, props.isLoading, props.status]);
 
@@ -29,6 +131,7 @@ const LessonDisplay = (props) => {
 
   return (
     <div className={classes.lesson}>
+      <canvas ref={canvasRef} className={classes.canvas} />
       <button className={classes.button} onClick={pageBackHandler}>
         <FontAwesomeIcon icon={faChevronLeft} />
       </button>
@@ -45,4 +148,4 @@ const LessonDisplay = (props) => {
   );
 };
 
-export default LessonDisplay;
+export default React.memo(LessonDisplay);
